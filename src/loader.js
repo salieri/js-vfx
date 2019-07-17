241bb4da2f2ad9736a139dc35901a54b169a3e72
Aleksi Asikainen <aleksi.asikainen@solera.com>
2019-07-17 14:30:34 -0500

    Minor fixes

--- src/loader.js
+++ src/loader.js
@@ -2,8 +2,8 @@
 require.config(
 		{
 			paths	: {
-					'jquery'	: 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-					'jqueryui'	: 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min'
+					'jquery'	: '../vendor/js/jquery-1.10.2-min',
+					'jqueryui'	: '../vendor/js/jquery-ui-1.10.4-min'
 				},
 
 			shim	: {
