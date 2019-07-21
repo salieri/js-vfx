9158a9a8f28001df50d558866f81070d321d5606
Aleksi Asikainen <aleksi.asikainen@gmail.com>
2019-07-21 14:00:59 -0500

    New build

--- dist/sw.js
+++ dist/sw.js
@@ -2,31 +2,31 @@ importScripts('/js-vfx/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/js-vfx/_nuxt/181c63e9db341da0b10b.js",
-    "revision": "cebaf525a5dd36407fbe9be4f88a68d5"
+    "url": "/js-vfx/_nuxt/6d00fdf54d54674751bf.js",
+    "revision": "686763fc8252c3f875fa3ebd99f7d668"
   },
   {
-    "url": "/js-vfx/_nuxt/19dfaba1809d8b64f50b.js",
-    "revision": "6c99353d16d8a536348ea2e59019dc43"
+    "url": "/js-vfx/_nuxt/732b6de6d552ee767734.js",
+    "revision": "29ab0d9569242af9fd7394164deae242"
   },
   {
-    "url": "/js-vfx/_nuxt/395adb16c063c8b9f401.js",
-    "revision": "6e19d28da9c4bed50fa66280056c442d"
+    "url": "/js-vfx/_nuxt/7d8db862817e336d777c.js",
+    "revision": "7d7ace239053958163126f571cd4bd91"
   },
   {
-    "url": "/js-vfx/_nuxt/81d40940cf9aceae5755.js",
-    "revision": "e9d5fed45f5678ff036b43699dc11242"
+    "url": "/js-vfx/_nuxt/8095dc8ca560027ce867.js",
+    "revision": "ce73884a3423a747d9f87f8b4502d0a1"
   },
   {
-    "url": "/js-vfx/_nuxt/f522e769adb40ce3da06.js",
-    "revision": "580ac2d396699b7a5cd189303842003c"
+    "url": "/js-vfx/_nuxt/e03e0674231c6abc7b30.js",
+    "revision": "14aba0cf68ab6b61a20259d5113c4f30"
   },
   {
-    "url": "/js-vfx/_nuxt/fc957fb92a957428fb3c.js",
-    "revision": "e55909f0ba2d4c32ca74b0281110fb6d"
+    "url": "/js-vfx/_nuxt/edef00f2169f188af6e9.js",
+    "revision": "9f8de85572fad586b05e8e33bbd95c01"
   }
 ], {
-  "cacheId": "nuxt",
+  "cacheId": "js-vfx",
   "directoryIndex": "/",
   "cleanUrls": false
 })
